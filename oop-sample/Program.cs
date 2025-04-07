e42e936f1fdbf4dac63fe4704a0ccd324da81e59
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-07 17:25:26 -0500

    feat(main): add main behavior.

--- oop-sample/Program.cs
+++ oop-sample/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using oop_sample.Procurement.Domain.Model.Aggregates;
+using oop_sample.Procurement.Domain.Model.ValueObjects;
+using oop_sample.SCM.Domain.Model.Aggregates;
+using oop_sample.SCM.Domain.Model.ValueObjects;
+using oop_sample.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+// Create a new Supplier
+var supplierAddress = new Address("Main St", "100", "New York", null, "62701", "United States");
+var supplier = new Supplier("Microsoft, Inc.", supplierAddress);
+
+
+// Create a new Purchase Order
+
+Console.WriteLine("Creating purchase order...");
+
+var purchaseOrder = new PurchaseOrder("PO001",
+    supplier.SupplierId, DateTime.Now, "USD");
+
+// Add items to the purchase order
+purchaseOrder
+    .AddItem(ProductId.New(), 10,25.99m)
+    .AddItem(ProductId.New(), 5, 15.99m);
+
+Console.WriteLine($"Purchase Order ID: {purchaseOrder.OrderNumber} created for supplier {supplier.Name}");
+
+foreach (var item in purchaseOrder.Items)
+    Console.WriteLine($"Item Subtotal: {item.CalculateSubtotal().Amount} {item.CalculateSubtotal().Currency}");
+// Calculate the total amount of the purchase order
+Console.WriteLine($"Total: {purchaseOrder.CalculateTotal().Amount} {purchaseOrder.CalculateTotal().Currency}");
+    
